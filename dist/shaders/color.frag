ac83c6ffdd145f93bce996bf3b2c399dc9b0b8ce
ahmedx3 <ahmed.moustafaa@outlook.com>
2019-10-27 16:41:14 +0200

    First Requirment (Sphere)

---
+++ dist/shaders/color.frag
@@ -0,0 +1,29 @@
+#version 300 es
+precision highp float;
+
+in vec4 vertexColor; // An input received from the vertex shader. Since the vertex shader only send 3 colors (one for each vertex), the rasterizer will interpolate the 3 values to get values for the fragments in the middle of the triangle
+// Info: the variables sending data between the vertex and fragment shader are called Interpolators
+
+out vec4 color;
+
+in vec3 vertexPosition;
+
+uniform mat4 tint; // This tint variable will be used to give us more control over the output color
+
+float xColor;
+float yColor;
+float zColor;
+
+void main(){
+
+    xColor = ( vertexPosition.x + 1.0 ) / 2.0 * 1.0;
+    yColor = ( vertexPosition.y + 1.0 ) / 2.0 * 1.0;
+    zColor = ( vertexPosition.z + 1.0 ) / 2.0 * 1.0;
+
+    //xColor = vertexPosition.x;
+    //yColor = vertexPosition.y;
+    //zColor = vertexPosition.z;
+
+    color = vec4( xColor , yColor , zColor , 1.0 );
+    //color = tint * vertexColor; // Send our interpolated color
+}
---
+++ src/common/mesh-utils.ts
@@ -0,0 +1,261 @@
+import Mesh from './mesh';
+import * as OBJ from 'webgl-obj-loader';
+
+// This file contain some helper classes to create simple meshes
+
+const BLACK = [0, 0, 0, 255];
+const RED = [255, 0, 0, 255];
+const GREEN = [0, 255, 0, 255];
+const BLUE = [0, 0, 255, 255];
+const YELLOW = [255, 255, 0, 255];
+const MAGENTA = [255, 0, 255, 255];
+const CYAN = [0, 255, 255, 255];
+const WHITE = [255, 255, 255, 255];
+
+function createMesh(gl: WebGL2RenderingContext): Mesh {
+    return new Mesh(gl, [
+        { attributeLocation: 0, buffer: "positions", size: 3, type: gl.FLOAT, normalized: false, stride: 0, offset: 0 },
+        { attributeLocation: 1, buffer: "colors", size: 4, type: gl.UNSIGNED_BYTE, normalized: true, stride: 0, offset: 0 }
+    ]);
+}
+
+export function ColoredPlane(gl: WebGL2RenderingContext): Mesh {
+    let mesh = createMesh(gl);
+    mesh.setBufferData("positions", new Float32Array([
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.5,  0.5, 0.0,
+        -0.5,  0.5, 0.0,
+    ]), gl.STATIC_DRAW);
+    mesh.setBufferData("colors", new Uint8Array([
+        255,   0,   0, 255,
+          0, 255,   0, 255,
+          0,   0, 255, 255,
+        255,   0, 255, 255,
+    ]), gl.STATIC_DRAW);
+    mesh.setElementsData(new Uint32Array([
+        0, 1, 2,
+        2, 3, 0
+    ]), gl.STATIC_DRAW);
+    return mesh
+}
+
+export function ColoredCube(gl: WebGL2RenderingContext): Mesh {
+    let mesh = createMesh(gl);
+    mesh.setBufferData("positions", new Float32Array([
+        //Upper Face
+        -1,  1, -1,
+        -1,  1,  1,
+         1,  1,  1,
+         1,  1, -1,
+        //Lower Face
+        -1, -1, -1,
+         1, -1, -1,
+         1, -1,  1,
+        -1, -1,  1,
+        //Right Face
+         1, -1, -1,
+         1,  1, -1,
+         1,  1,  1,
+         1, -1,  1,
+        //Left Face
+        -1, -1, -1,
+        -1, -1,  1,
+        -1,  1,  1,
+        -1,  1, -1,
+        //Front Face
+        -1, -1,  1,
+         1, -1,  1,
+         1,  1,  1,
+        -1,  1,  1,
+        //Back Face
+        -1, -1, -1,
+        -1,  1, -1,
+         1,  1, -1,
+         1, -1, -1
+    ]), gl.STATIC_DRAW);
+    mesh.setBufferData("colors", new Uint8Array([
+        //Upper Face
+        ...RED, ...RED, ...RED, ...RED,
+        //Lower Face
+        ...GREEN, ...GREEN, ...GREEN, ...GREEN,
+        //Right Face
+        ...BLUE, ...BLUE, ...BLUE, ...BLUE,
+        //Left Face
+        ...YELLOW, ...YELLOW, ...YELLOW, ...YELLOW,
+        //Front Face
+        ...MAGENTA, ...MAGENTA, ...MAGENTA, ...MAGENTA,
+        //Back Face
+        ...CYAN, ...CYAN, ...CYAN, ...CYAN,
+    ]), gl.STATIC_DRAW);
+    mesh.setElementsData(new Uint32Array([
+        //Upper Face
+        0, 1, 2, 2, 3, 0,
+        //Lower Face
+        4, 5, 6, 6, 7, 4,
+        //Right Face
+        8, 9, 10, 10, 11, 8,
+        //Left Face
+        12, 13, 14, 14, 15, 12,
+        //Front Face
+        16, 17, 18, 18, 19, 16,
+        //Back Face
+        20, 21, 22, 22, 23, 20, 
+    ]), gl.STATIC_DRAW);
+    return mesh;
+}
+
+export function WhiteCube(gl: WebGL2RenderingContext): Mesh {
+    let mesh = createMesh(gl);
+    mesh.setBufferData("positions", new Float32Array([
+        //Upper Face
+        -1,  1, -1,
+        -1,  1,  1,
+         1,  1,  1,
+         1,  1, -1,
+        //Lower Face
+        -1, -1, -1,
+         1, -1, -1,
+         1, -1,  1,
+        -1, -1,  1,
+        //Right Face
+         1, -1, -1,
+         1,  1, -1,
+         1,  1,  1,
+         1, -1,  1,
+        //Left Face
+        -1, -1, -1,
+        -1, -1,  1,
+        -1,  1,  1,
+        -1,  1, -1,
+        //Front Face
+        -1, -1,  1,
+         1, -1,  1,
+         1,  1,  1,
+        -1,  1,  1,
+        //Back Face
+        -1, -1, -1,
+        -1,  1, -1,
+         1,  1, -1,
+         1, -1, -1
+    ]), gl.STATIC_DRAW);
+    mesh.setBufferData("colors", new Uint8Array([
+        //Upper Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+        //Lower Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+        //Right Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+        //Left Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+        //Front Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+        //Back Face
+        ...WHITE, ...WHITE, ...WHITE, ...WHITE,
+    ]), gl.STATIC_DRAW);
+    mesh.setElementsData(new Uint32Array([
+        //Upper Face
+        0, 1, 2, 2, 3, 0,
+        //Lower Face
+        4, 5, 6, 6, 7, 4,
+        //Right Face
+        8, 9, 10, 10, 11, 8,
+        //Left Face
+        12, 13, 14, 14, 15, 12,
+        //Front Face
+        16, 17, 18, 18, 19, 16,
+        //Back Face
+        20, 21, 22, 22, 23, 20, 
+    ]), gl.STATIC_DRAW);
+    return mesh;
+}
+
+export function LoadOBJMesh(gl: WebGL2RenderingContext, data: string){
+    let obj = new OBJ.Mesh(data);
+    let mesh = createMesh(gl);
+    mesh.setBufferData("positions", new Float32Array(obj.vertices), gl.STATIC_DRAW);
+    let colors = new Uint8Array(obj.vertices.length * 4 / 3);
+    colors.fill(255);
+    mesh.setBufferData("colors", colors, gl.STATIC_DRAW);
+    mesh.setElementsData(new Uint32Array(obj.indices), gl.STATIC_DRAW);
+    return mesh;
+}
+
+export function ColoredSphere(gl: WebGL2RenderingContext, verticalResolution: number=32, horizontalResolution: number=32): Mesh{
+    // TODO: Create a colored sphere mesh and return it
+    let mesh = createMesh(gl);
+
+    let radius = 2;
+
+    let vertexPositionData = [];
+    let normalData = [];
+    let textureCoordData = [];
+    let indexData = [];
+
+    // Calculate sphere vertex positions, normals, and texture coordinates.
+    for (let latNumber = 0; latNumber <= horizontalResolution; ++latNumber) {
+      let theta = latNumber * Math.PI / horizontalResolution;
+      let sinTheta = Math.sin(theta);
+      let cosTheta = Math.cos(theta);
+
+      for (let longNumber = 0; longNumber <= verticalResolution; ++longNumber) {
+        let phi = longNumber * 2 * Math.PI / verticalResolution;
+        let sinPhi = Math.sin(phi);
+        let cosPhi = Math.cos(phi);
+
+        let x = cosPhi * sinTheta;
+        let y = cosTheta;
+        let z = sinPhi * sinTheta;
+
+        let u = 1 - (longNumber / verticalResolution);
+        let v = 1 - (latNumber / horizontalResolution);
+
+        vertexPositionData.push(radius * x);
+        vertexPositionData.push(radius * y);
+        vertexPositionData.push(radius * z);
+
+        normalData.push(x);
+        normalData.push(y);
+        normalData.push(z);
+
+        textureCoordData.push(u);
+        textureCoordData.push(v);
+      }
+    }
+
+    // Calculate sphere indices.
+    for (let latNumber = 0; latNumber < horizontalResolution; ++latNumber) {
+      for (let longNumber = 0; longNumber < verticalResolution; ++longNumber) {
+        let first = (latNumber * (verticalResolution + 1)) + longNumber;
+        let second = first + verticalResolution + 1;
+
+        indexData.push(first);
+        indexData.push(second);
+        indexData.push(first + 1);
+
+        indexData.push(second);
+        indexData.push(second + 1);
+        indexData.push(first + 1);
+      }
+    }
+
+    let vertexPositionDataFinal = new Float32Array(vertexPositionData);
+    let normalDataFinal = new Float32Array(normalData);
+    let textureCoordDataFinal = new Float32Array(textureCoordData);
+    let indexDataFinal = new Uint16Array(indexData);
+
+    mesh.setBufferData("positions" , vertexPositionDataFinal , gl.STATIC_DRAW);
+
+    let colorArray = [];
+    for (let i = 0; i < vertexPositionDataFinal.length; i++) {
+        colorArray.push(BLUE);
+    }
+
+    let colorArrayFinal = new Uint8Array(colorArray);
+
+    //mesh.setBufferData("colors", colorArrayFinal, gl.STATIC_DRAW);
+
+    mesh.setElementsData(indexDataFinal , gl.STATIC_DRAW);
+
+    return mesh;
+}
